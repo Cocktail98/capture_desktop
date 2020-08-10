33f189d62c3aa84339b61635e5c3e156a0d27708
Cocktail98 <guangchen@mail.ynu.edu.cn>
2020-08-10 09:05:52 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,53 @@
+from PIL import ImageGrab
+import numpy as np
+from flask import Flask, render_template, Response
+import cv2
+
+
+class VideoCamera(object):
+    def __init__(self):
+        # 通过opencv获取实时视频流
+        # self.video = cv2.VideoCapture(1)
+        pass
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        img = ImageGrab.grab(
+            bbox=(100, 10, 400, 700))  # bbox specifies specific region (bbox= x,y,width,height *starts top-left)
+        img_np = np.array(img)  # this is the array obtained from conversion
+        # frame = cv2.cvtColor(img_np, cv2.COLOR_GRAY2BGR)
+        # print(type(img))
+
+        # success, image = self.video.read()
+        # 因为opencv读取的图片并非jpeg格式，因此要用motion JPEG模式需要先将图片转码成jpg格式图片
+        ret, jpeg = cv2.imencode('.jpg', img_np)
+        return jpeg.tobytes()
+
+
+app = Flask(__name__)
+
+
+@app.route('/')  # 主页
+def index():
+    # jinja2模板，具体格式保存在index.html文件中
+    return render_template('index.html')
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        # 使用generator函数输出视频流， 每次请求输出的content类型是image/jpeg
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@app.route('/video_feed')  # 这个地址返回视频流响应
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port = 5000)
